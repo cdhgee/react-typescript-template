7eca3649f275c2b034adcbbe0d4d3eebf5495017
David Gee <git@allpowerfuldave.com>
2018-04-05 16:55:04 -0500

    Add Babel 7 config file

---
+++ .babelrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    ["@babel/preset-typescript",],
+    [
+      "@babel/env",
+      {
+        targets: {
+          node: "current",
+        },
+      },
+    ],
+  ],
+};
